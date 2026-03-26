fffb3aaa5af4092b978f6120f9ae9cae70d36e2b
Nikita Golubev <golubev.nikita@gmail.com>
2026-03-26 16:18:12 +0300

    удалил public

--- public/img/im_resize.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-im = Image.open("logo.png")
-im = im.convert("RGB")
-im.thumbnail((355, 50), Image.LANCZOS)  # вписать в 1200x1200
-im.save("logo2.png", quality=85, optimize=True)
